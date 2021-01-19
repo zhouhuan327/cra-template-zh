c3fd2c637e66ac82e995abe1316dcc7376d04485
zhouhuan <zsean21598@163.com>
2021-01-19 11:26:16 +0800

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [n, useN] = useState<number>(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
