27b64ecdd1e635ea684af992b403698f2b74aa67
zhouhuan <zsean21598@163.com>
2021-01-21 11:52:51 +0800

    优化sync脚本

--- sync-script/index.js
+++ sync-script/index.js
@@ -1,52 +1,62 @@
 const hyperquest = require("hyperquest");
+const https = require("https");
 const unpack = require("tar-pack").unpack;
 const fs = require("fs-extra");
 const path = require("path");
 const root = process.cwd();
-const tempFolderName = "temp";
+const tempFolderName = "tempFolder";
 const chalk = require("chalk");
-// npm下载地址,不是github地址
-const packageURL =
-  "https://registry.npmjs.org/cra-template-redux/-/cra-template-redux-1.0.2.tgz";
+const log = console.log;
 async function run() {
-  console.log(chalk.bgGreen("current template url:" + packageURL));
-  if (!checkIsInRoot()) {
-    return;
-  }
-  const templatePath = path.join(root, "sync-script", tempFolderName);
-  if (!fs.existsSync(templatePath)) {
-    let stream;
-    if (/^http/.test(packageURL)) {
-      console.log(chalk.green("pulling template..."));
-      stream = hyperquest(packageURL);
+  try {
+    log(chalk.bgMagenta("start sync"));
+    const packageURL = await getTemplateUrl();
+    log(chalk.magenta(`repo address: ${packageURL}`));
+    if (!checkIsInRoot()) {
+      return;
     }
-    await extractStream(stream, path.join(root, "sync-script", tempFolderName));
+    const templatePath = path.join(root, "sync-script", tempFolderName);
+    if (!fs.existsSync(templatePath)) {
+      let stream;
+      if (/^http/.test(packageURL)) {
+        log(chalk.green("pulling template..."));
+        stream = hyperquest(packageURL);
+      }
+      log(chalk.green("unpack template file..."));
+      await extractStream(
+        stream,
+        path.join(root, "sync-script", tempFolderName),
+      );
+    }
+
+    copyFiles(templatePath);
+
+    fs.removeSync(path.join(root, "sync-script", tempFolderName));
+  } catch (e) {
+    chalk.red(e);
   }
-
-  copyFiles(templatePath);
-
-  fs.removeSync(path.join(root, "sync-script", tempFolderName));
 }
+// 复制配置文件
 function copyFiles(templatePath) {
   const fileNames = [
     ".eslintrc.js",
     ".prettierrc",
-    ".prettierrcignore",
+    ".prettierignore",
     ".stylelintrc.json",
     "tsconfig.json",
   ];
   for (let filename of fileNames) {
-    const fileExists = fs.existsSync(path.join(templatePath, filename));
-    if (fileExists) {
-      fs.copySync(path.join(templatePath, filename), path.join(root, filename));
+    const filePath = path.join(templatePath, "template", filename);
+    if (fs.existsSync(filePath)) {
+      fs.copySync(filePath, path.join(root, filename));
     } else {
-      console.log(chalk.bgYellow(filename + " not exist in template"));
+      log(chalk.bgYellow(filename + " not exist in template"));
     }
   }
 }
+// 解压
 function extractStream(stream, dest) {
   return new Promise((resolve, reject) => {
-    console.log(chalk.green("unpack template file..."));
     stream.pipe(
       unpack(dest, err => {
         if (err) {
@@ -61,10 +71,31 @@ function extractStream(stream, dest) {
 function checkIsInRoot() {
   const packageExists = fs.existsSync(path.join(root, "package.json"));
   if (!packageExists) {
-    console.log(chalk.bgRed("package不存在"));
     throw new Error("package不存在");
   }
   return true;
 }
 
+function getTemplateUrl() {
+  // 返回的json信息中包含版本号
+  const url = "https://registry.npmjs.org/cra-template-zh";
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, resp => {
+        let data = "";
+        resp.on("data", chunk => {
+          data += chunk;
+        });
+        resp.on("end", () => {
+          const info = JSON.parse(data);
+          const version = info && info["dist-tags"] && info["dist-tags"].latest;
+          const url = `https://registry.npmjs.org/cra-template-zh/-/cra-template-zh-${version}.tgz`;
+          resolve(url);
+        });
+      })
+      .on("error", err => {
+        reject(err.message);
+      });
+  });
+}
 run();
